cd33a0f6185148a8cab9acb42f6f911610621698
Jay Noonan <noonanjn@bc.edu>
2022-12-04 12:48:37 -0500

    Initial Commit

---
+++ WhatToWatch/WhatToWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatToWatchApp.swift
+//  WhatToWatch
+//
+//  Created by Jay Noonan on 12/4/22.
+//
+
+import SwiftUI
+
+@main
+struct WhatToWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
